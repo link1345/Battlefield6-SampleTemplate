1b23eaabc6966c0858d01274888a98b34cac04f0
link1345 <testkusa@gmail.com>
2025-10-27 09:45:32 +0900

    feat: add github actions

---
+++ mods/Script.ts
@@ -0,0 +1,6 @@
+import * as modlib from "modlib";
+
+
+export function OnPlayerJoinGame(eventPlayer: mod.Player) {
+    modlib.ShowNotificationMessage(mod.Message(mod.stringkeys.hello, eventPlayer), eventPlayer);
+}
